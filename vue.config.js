3376768235d77ab466e02b4c2cb54f35196c1d31
username / Elisa + Morreale <elisamorreale@gmail.com>
2024-12-10 16:20:48 +0100

    impostazioni per sviluppo

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,8 @@
-// import * as path from 'path'
-
-// module.exports = {
-//   // Definisce il percorso pubblico per il deploy su GitHub Pages
-//   publicPath: '/progetto-astici-CISMAR/',
-
-//   // Configurazione di Webpack
-//   configureWebpack: {
-//     resolve: {
-//       alias: {
-//         // Aggiungi un alias per la cartella src
-//         '@': path.resolve(__dirname, 'src'),
-//       },
-//     },
-//   },
-
-//   // Altri eventuali parametri di configurazione per Vue CLI
-//   // (ad esempio per il dev server, per la gestione delle risorse, ecc.)
-// }
-
 import * as path from 'path'
 
 module.exports = {
-  // Definisce il percorso pubblico per il deploy su GitHub Pages
-  publicPath: process.env.NODE_ENV === 'production' ? '/progetto-astici-CISMAR/' : '/',
+  // Definisce il percorso pubblico per il deploy su GitHub Pages (come hai già fatto)
+  publicPath: '/progetto-astici-CISMAR/',
 
   // Configurazione di Webpack
   configureWebpack: {
@@ -34,10 +14,6 @@ module.exports = {
     },
   },
 
-  // Configurazione del dev server (opzionale)
-  devServer: {
-    port: 5173, // Cambia la porta se necessario
-  },
-
   // Altri eventuali parametri di configurazione per Vue CLI
+  // (ad esempio per il dev server, per la gestione delle risorse, ecc.)
 }
